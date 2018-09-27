58fa6e4e4d261c397df29c223c3e643d1cb9244c
zhongjianbin <iszhongjianbin@163.com>
2018-09-27 10:36:05 +0800

    for data structure practice

--- src/com/Demon.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-public class Demon {
-
-    public static void main(String[] args){
-        System.out.println("kobe");
-    }
-}
--- src/com/javascript/w3school/Test.java
+++
@@ -1,4 +0,0 @@
-package com.javascript.w3school;
-
-public class Test {
-}
